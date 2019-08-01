fae1f3514e0bc4baffdb0b89ae1ed4dd7de52278
Krzysztof Kruk <krukkrz@github.com>
2019-08-01 20:58:25 +0200

    excel2json finished

--- src/app/modules/excel2json/excel2json.module.ts
+++ src/app/modules/excel2json/excel2json.module.ts
@@ -1,9 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Excel2jsonComponent } from './excel2json/excel2json.component';
 import { Excel2jsonRouting } from './excel2json-routing.module';
+import {MatCardModule} from '@angular/material/card';
+import {MatGridListModule} from '@angular/material/grid-list';
+import { NgxJsonViewerModule } from 'ngx-json-viewer';
+
 
 @NgModule({
   declarations: [Excel2jsonComponent],
-  imports: [Excel2jsonRouting],
+  imports: [
+      Excel2jsonRouting,
+      MatCardModule,
+      MatGridListModule,
+      NgxJsonViewerModule
+    ],
 })
 export class Excel2jsonModule{}
--- src/app/modules/excel2json/excel2json/excel2json.component.ts
+++ src/app/modules/excel2json/excel2json/excel2json.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as XLSX from 'xlsx';
+
 
 @Component({
   selector: 'app-excel2json',
@@ -11,5 +13,26 @@ export class Excel2jsonComponent implements OnInit {
 
   ngOnInit() {
   }
+
+  arrayBuffer:any;
+  file:File;
+  data: any
+  incomingfile(event) {
+    this.file= event.target.files[0]; 
+    let fileReader = new FileReader();
+          fileReader.onload = (e) => {
+              this.arrayBuffer = fileReader.result;
+              var data = new Uint8Array(this.arrayBuffer);
+              var arr = new Array();
+              for(var i = 0; i != data.length; ++i) arr[i] = String.fromCharCode(data[i]);
+              var bstr = arr.join("");
+              var workbook = XLSX.read(bstr, {type:"binary"});
+              var first_sheet_name = workbook.SheetNames[0];
+              var worksheet = workbook.Sheets[first_sheet_name];
+              this.data = XLSX.utils.sheet_to_json(worksheet,{raw:true})
+              console.log(this.data);
+          }
+          fileReader.readAsArrayBuffer(this.file);
+    }
 
 }
